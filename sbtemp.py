422c3809d6c192ef65e007291cb04452384faea1
Jason Costomiris <jcostom@jasons.org>
2022-02-22 14:05:15 -0500

    Cleanup post flake8 intro

--- sbtemp.py
+++ sbtemp.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import os
-import json
 import time
 import asyncio
 import requests
@@ -11,9 +10,9 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 plugIP = os.getenv('plugIP')
 dayLow = int(os.getenv('dayLow', 65))
-dayHigh = int(os.getenv('dayHigh',69))
+dayHigh = int(os.getenv('dayHigh', 69))
 nightLow = int(os.getenv('nightLow', 62))
-nightHigh = int(os.getenv('nightHigh',66))
+nightHigh = int(os.getenv('nightHigh', 66))
 sleepTime = int(os.getenv('sleepTime', 300))
 nightBegin = os.getenv('nightBegin')
 nightEnd = os.getenv('nightEnd')
@@ -25,31 +24,40 @@ influxToken = os.getenv('influxToken')
 influxURL = os.getenv('influxURL')
 influxMeasurement = os.getenv('influxMeasurement')
 
+
 def c2f(celsius):
     return (celsius * 9/5) + 32
+
 
 def readSensor(sbURL, sbHeaders):
     r = requests.get(sbURL, headers=sbHeaders)
     # return array of (degF, rHum)
-    return (round(c2f(r.json()['body']['temperature']),1), r.json()['body']['humidity'])
+    return (round(c2f(r.json()['body']['temperature']), 1),
+            r.json()['body']['humidity'])
+
 
 def writeLogEntry(message, status):
-    print(time.strftime("[%d %b %Y %H:%M:%S %Z]", time.localtime()) + " {}: {}".format(message, status))
+    print(time.strftime("[%d %b %Y %H:%M:%S %Z]",
+          time.localtime()) + " {}: {}".format(message, status))
+
 
 def checkTimeRange(time, timeRange):
     if timeRange[1] < timeRange[0]:
         return time >= timeRange[0] or time <= timeRange[1]
     return timeRange[0] <= time <= timeRange[1]
 
+
 async def plugOff(ip):
     p = SmartPlug(ip)
     await p.update()
     await p.turn_off()
 
+
 async def plugOn(ip):
     p = SmartPlug(ip)
     await p.update()
     await p.turn_on()
+
 
 async def readConsumption(ip):
     p = SmartPlug(ip)
@@ -57,17 +65,19 @@ async def readConsumption(ip):
     watts = await p.current_consumption()
     return(watts)
 
+
 def main():
     url = "/".join(
         ("https://api.switch-bot.com/v1.0/devices",
-        DEVID,
-        "status")
+         DEVID,
+         "status")
     )
-    headers = { 'Authorization': APIKEY }
-    influxClient = InfluxDBClient(url=influxURL,token=influxToken,org=influxOrg)
+    headers = {'Authorization': APIKEY}
+    influxClient = InfluxDBClient(url=influxURL, token=influxToken,
+                                  org=influxOrg)
     write_api = influxClient.write_api(write_options=SYNCHRONOUS)
     timeRange = (nightBegin, nightEnd)
-    writeLogEntry('Startup','')
+    writeLogEntry('Startup', '')
     while True:
         (degF, rH) = readSensor(url, headers)
         watts = asyncio.run(readConsumption(plugIP))
@@ -106,6 +116,7 @@ def main():
                 # no state change required
                 pass
         time.sleep(sleepTime)
+
 
 if __name__ == "__main__":
     main()
