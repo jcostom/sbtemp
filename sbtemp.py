95f079e4f5f09ab577034c047203df2179ed10ce
Jason Costomiris <jcostom@jasons.org>
2022-04-16 01:18:22 -0400

    Refactor to better implement PEP8 standards

--- sbtemp.py
+++ sbtemp.py
@@ -9,27 +9,25 @@ from kasa import SmartPlug
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-plugIP = os.getenv('plugIP')
-dayLow = int(os.getenv('dayLow', 65))
-dayHigh = int(os.getenv('dayHigh', 69))
-nightLow = int(os.getenv('nightLow', 62))
-nightHigh = int(os.getenv('nightHigh', 66))
-sleepTime = int(os.getenv('sleepTime', 300))
-nightBegin = os.getenv('nightBegin')
-nightEnd = os.getenv('nightEnd')
+PLUG_IP = os.getenv('PLUG_IP')
+DAY_LOW = int(os.getenv('DAY_LOW', 65))
+DAY_HIGH = int(os.getenv('DAY_HIGH', 69))
+NIGHT_LOW = int(os.getenv('NIGHT_LOW', 62))
+NIGHT_HIGH = int(os.getenv('NIGHT_HIGH', 66))
+NIGHT_BEGIN = os.getenv('NIGHT_BEGIN')
+NIGHT_END = os.getenv('NIGHT_END')
+SLEEP_TIME = int(os.getenv('SLEEP_TIME', 300))
 APIKEY = os.getenv('APIKEY')
 DEVID = os.getenv('DEVID')
-influxBucket = os.getenv('influxBucket')
-influxOrg = os.getenv('influxOrg')
-influxToken = os.getenv('influxToken')
-influxURL = os.getenv('influxURL')
-influxMeasurement = os.getenv('influxMeasurement')
+INFLUX_BUCKET = os.getenv('INFLUX_BUCKET')
+INFLUX_ORG = os.getenv('INFLUX_ORG')
+INFLUX_TOKEN = os.getenv('INFLUX_TOKEN')
+INFLUX_URL = os.getenv('INFLUX_URL')
+INFLUX_MEASUREMENT = os.getenv('INFLUX_MEASUREMENT')
 DEBUG = int(os.getenv('DEBUG', 0))
 
-version = '2.3'
-UA_STRING = "/".join(
-    ["sbtemp.py", version]
-)
+VER = '2.5'
+UA_STRING = f"sbtemp.py/{VER}"
 
 # Setup logger
 logger = logging.getLogger()
@@ -47,36 +45,36 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 
-def c2f(celsius):
+def c2f(celsius: float) -> float:
     return (celsius * 9/5) + 32
 
 
-def readSensor(sbURL, sbHeaders):
-    r = requests.get(sbURL, headers=sbHeaders)
+def read_sensor(sb_url: str, sb_headers: dict) -> list:
+    r = requests.get(sb_url, headers=sb_headers)
     # return array of (degF, rHum)
-    return (round(c2f(r.json()['body']['temperature']), 1),
-            r.json()['body']['humidity'])
+    return [round(c2f(r.json()['body']['temperature']), 1),
+            r.json()['body']['humidity']]
 
 
-def checkTimeRange(time, timeRange):
-    if timeRange[1] < timeRange[0]:
-        return time >= timeRange[0] or time <= timeRange[1]
-    return timeRange[0] <= time <= timeRange[1]
+def check_time_range(time: str, time_range: list) -> bool:
+    if time_range[1] < time_range[0]:
+        return time >= time_range[0] or time <= time_range[1]
+    return time_range[0] <= time <= time_range[1]
 
 
-async def plugOff(ip):
+async def plug_off(ip: str) -> None:
     p = SmartPlug(ip)
     await p.update()
     await p.turn_off()
 
 
-async def plugOn(ip):
+async def plug_on(ip: str) -> None:
     p = SmartPlug(ip)
     await p.update()
     await p.turn_on()
 
 
-async def readConsumption(ip):
+async def read_consumption(ip: str) -> float:
     p = SmartPlug(ip)
     await p.update()
     watts = await p.current_consumption()
@@ -84,55 +82,51 @@ async def readConsumption(ip):
 
 
 def main():
-    url = "/".join(
-        ("https://api.switch-bot.com/v1.0/devices",
-         DEVID,
-         "status")
-    )
+    url = f"https://api.switch-bot.com/v1.0/devices/{DEVID}/status"
     headers = {'Authorization': APIKEY}
-    influxClient = InfluxDBClient(url=influxURL, token=influxToken,
-                                  org=influxOrg)
-    write_api = influxClient.write_api(write_options=SYNCHRONOUS)
-    timeRange = (nightBegin, nightEnd)
+    influx_client = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN,
+                                   org=INFLUX_ORG)
+    write_api = influx_client.write_api(write_options=SYNCHRONOUS)
+    time_range = (NIGHT_BEGIN, NIGHT_END)
     logger.info(f"Startup: {UA_STRING}")
     while True:
-        (degF, rH) = readSensor(url, headers)
-        watts = asyncio.run(readConsumption(plugIP))
+        (deg_f, rel_hum) = read_sensor(url, headers)
+        watts = asyncio.run(read_consumption(PLUG_IP))
         record = [
             {
-                "measurement": influxMeasurement,
+                "measurement": INFLUX_MEASUREMENT,
                 "fields": {
-                    "degF": degF,
-                    "rH": rH,
+                    "degF": deg_f,
+                    "rH": rel_hum,
                     "power": watts
                 }
             }
         ]
-        write_api.write(bucket=influxBucket, record=record)
+        write_api.write(bucket=INFLUX_BUCKET, record=record)
         now = strftime("%H:%M", localtime())
-        if checkTimeRange(now, timeRange):
+        if check_time_range(now, time_range):
             # We are in night schedule
-            if degF < nightLow:
-                asyncio.run(plugOn(plugIP))
-                logger.info(f"Night: Change state to ON, temp: {degF}")
-            elif degF > nightHigh:
-                asyncio.run(plugOff(plugIP))
-                logger.info(f"Night: Change state to OFF, temp: {degF}")
+            if deg_f < NIGHT_LOW:
+                asyncio.run(plug_on(PLUG_IP))
+                logger.info(f"Night: Change state to ON, temp: {deg_f}")
+            elif deg_f > NIGHT_HIGH:
+                asyncio.run(plug_off(PLUG_IP))
+                logger.info(f"Night: Change state to OFF, temp: {deg_f}")
             else:
                 # no state change required
                 pass
         else:
             # we are in day schedule
-            if degF < dayLow:
-                asyncio.run(plugOn(plugIP))
-                logger.info(f"Day: Change state to ON, temp: {degF}")
-            elif degF > dayHigh:
-                asyncio.run(plugOff(plugIP))
-                logger.info(f"Day: Change state to OFF, temp: {degF}")
+            if deg_f < DAY_LOW:
+                asyncio.run(plug_on(PLUG_IP))
+                logger.info(f"Day: Change state to ON, temp: {deg_f}")
+            elif deg_f > DAY_HIGH:
+                asyncio.run(plug_off(PLUG_IP))
+                logger.info(f"Day: Change state to OFF, temp: {deg_f}")
             else:
                 # no state change required
                 pass
-        sleep(sleepTime)
+        sleep(SLEEP_TIME)
 
 
 if __name__ == "__main__":
